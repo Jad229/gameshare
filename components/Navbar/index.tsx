26c9770f85e452d4b5ae76843d981a96a0e6252b
Jad229 <jad229@njit.edu>
2024-03-07 18:36:16 -0500

    added navbar component

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,3 +1,33 @@
+import Link from "next/link";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-between items-center p-4">
+      {/* brand */}
+      <div>
+        <Link href={"/"} className="text-2xl tracking-widest font-bold">
+          Game<span className="text-sky-500">Share</span>
+        </Link>
+      </div>
+
+      {/* search bar */}
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="rounded-full p-2 bg-zinc-800 shadow-md w-80 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-opacity-50 transition duration-300 ease-in-out"
+        />
+      </div>
+
+      {/* navigation */}
+      <ul className="flex justify-between gap-5 items-center">
+        <li>
+          <Link href={"/profile"}>Profile</Link>
+        </li>
+        <li>
+          <Link href={"/library"}>My Library</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
