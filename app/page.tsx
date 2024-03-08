94c2c1482a9d69ddacf43d76940fc4a1faa74827
Jad229 <jad229@njit.edu>
2024-03-07 20:22:37 -0500

    Add GameCard component and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import { GameCard } from "@/components";
+
 export default function Home() {
-  return <h1>GameShare</h1>;
+  return (
+    <main>
+      {/* explore section */}
+      <section>
+        <h1 className="text-5xl font-bold text-sky-500">Explore</h1>
+        <div className="flex flex-wrap justify-center items-center gap-4 overflow-scroll p-4">
+          {/* game card */}
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+        </div>
+      </section>
+    </main>
+  );
 }
---
+++ components/GameCard/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+
+export default function index() {
+  return (
+    <div className="flex flex-col items-center justify-center w-80 gap-3 bg-zinc-700 rounded-lg shadow-xl">
+      <div className="w-full h-40 overflow-hidden relative">
+        <Image
+          src="https://via.placeholder.com/200"
+          alt="game cover"
+          className="min-h-full object-cover rounded-md"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="p-4">
+        <h2 className="text-xl font-bold">Game Title</h2>
+      </div>
+      <div className="p-4 w-full flex justify-between items-center ">
+        <button className="bg-violet-500 py-2 px-4 rounded-lg transition-all hover:scale-105 shadow-xl ">
+          More Info
+        </button>
+        <button className="bg-sky-500 py-3 px-4 rounded-lg text-black transition-all hover:scale-105 shadow-xl">
+          <FaPlus />
+        </button>
+      </div>
+    </div>
+  );
+}
