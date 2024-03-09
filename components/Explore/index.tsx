443e8168dcd9e04b1800ab1b2fe7cd4ef956e9d9
Jad229 <jad229@njit.edu>
2024-03-09 17:50:18 -0500

    edited explore section width

--- components/Explore/index.tsx
+++ components/Explore/index.tsx
@@ -11,7 +11,7 @@ export default async function Explore({ page }: props) {
   const games = await fetchGameList(page);
 
   return (
-    <section className="flex flex-col justify-center items-center gap-10 pt-10">
+    <section className="flex flex-col justify-center items-center gap-10 pt-10 w-10/12 mx-auto">
       <h1 className="text-5xl font-bold text-transparent bg-gradient-to-r from-sky-400 via-violet-500 to-fuchsia-500 bg-clip-text inline-block">
         Explore
       </h1>
