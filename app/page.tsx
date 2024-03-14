60efb88d09914f90d1535c93ffe79eac5f8c0ad0
Jad229 <jad229@njit.edu>
2024-03-14 16:38:54 -0400

    installed vercel/postgres

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ export default async function Home({ searchParams }: props) {
   const page = searchParams?.page || 1;
   return (
     <main>
+      {/* <FriendsList /> */}
+      {/* <TopGame /> */}
       {/* explore section */}
       <Explore page={page} />
     </main>
